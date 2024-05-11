68ed23d2cc6d6379089321b96f6aff325e762c13
augin <as@augin.ru>
2024-05-11 13:57:21 +0300

    Update cams.php

--- www/cams.php
+++ www/cams.php
@@ -1,5 +1,24 @@
 <?php
-echo '{"cameras": ';
-echo shell_exec("yaml2json /opt/go2rtc/go2rtc.yaml | jq '.streams | keys_unsorted'");
-echo '}';
+// Убедитесь, что расширение php-yaml установлено и включено
+
+// Загрузка содержимого файла конфигурации nvr.yaml
+$nvrConfig = yaml_parse_file('nvr.yaml');
+
+// Проверка наличия пути к файлу в конфигурации
+if (isset($nvrConfig['go2rtc_config_path'])) {
+    $go2rtc_config_path = $nvrConfig['go2rtc_config_path'];
+    // Загрузка содержимого YAML файла по полученному пути
+    $yamlContents = yaml_parse_file($go2rtc_config_path);
+
+    // Проверка наличия раздела 'streams' и извлечение ключей
+    if (isset($yamlContents['streams'])) {
+        $streamsKeys = array_keys($yamlContents['streams']);
+        // Кодирование ключей в JSON и вывод
+        echo json_encode(['cameras' => $streamsKeys]);
+    } else {
+        echo "Раздел 'streams' не найден в YAML файле.";
+    }
+} else {
+    echo "Путь к YAML файлу не найден в файле конфигурации nvr.yaml.";
+}
 ?>
